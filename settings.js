636e8521acfb442cdeb568b20db6ab7b3f7e0d07
Darkjanu <108587475+Darkjanu@users.noreply.github.com>
2022-07-03 17:25:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,16 +19,16 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['+94788230710']
+global.premium = ['+94788230710']
+global.ownernomer = '+94788230710'
 global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.botname = 'Jᵃⁿᵘ ʙᴏᴛ @²⁰²²'
+global.footer = '©Jᵃⁿᵘ ʙᴏᴛ.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
+global.myweb = 'https://www.facebook.com/profile.php?id=100070412296277'
 global.packname = 'Gojo-Satoru'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
